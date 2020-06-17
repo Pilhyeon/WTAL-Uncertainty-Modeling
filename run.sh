d27e176ce4aa66bcf954093a61336cde54ecd716
Pilhyeon <justdreaming1114@gmail.com>
2020-06-17 15:06:51 +0900

    edit code

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ output_path="./outputs/BMUE"
 log_path="./logs/BMUE"
 seed=6
 
-CUDA_VISIBLE_DEVICES=2 python -W ignore ./main.py --model_path ${model_path} --output_path ${output_path} --log_path ${log_path} --seed ${seed}
+CUDA_VISIBLE_DEVICES=0 python -W ignore ./main.py --model_path ${model_path} --output_path ${output_path} --log_path ${log_path} --seed ${seed}
