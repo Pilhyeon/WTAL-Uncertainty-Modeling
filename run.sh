09daa0233d4546d5e7ad9a28a01b8027375a57cc
Pilhyeon Lee <lph1114@yonsei.ac.kr>
2020-09-17 17:30:52 +0900

    Code update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ output_path="./outputs/UM"
 log_path="./logs/UM"
 seed=0
 
-CUDA_VISIBLE_DEVICES=0 python -W ignore ./main.py --model_path ${model_path} --output_path ${output_path} --log_path ${log_path}
+CUDA_VISIBLE_DEVICES=0 python -W ignore ./main.py --model_path ${model_path} --output_path ${output_path} --log_path ${log_path} --seed ${seed}
